8b137ab13a3ac2111238b02ce39f27da50c4a834
am7330226-ctrl <am7330226@gmail.com>
2026-07-15 20:19:29 +0530

    refactor: change SPA fallback route to use middleware for broader request coverage

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post('/api/data', authenticate, (req, res) => {
 });
 
 // Fallback to index.html for SPA
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
