7eed8e5436c9823a024f0e4b2af04018e431083e
am7330226-ctrl <am7330226@gmail.com>
2026-08-03 12:40:33 +0530

    chore: untrack node_modules and dist, relax commitlint rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,12 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [0],
+    'type-empty': [0],
+    'subject-empty': [0],
+    'subject-full-stop': [0],
+    'subject-case': [0],
+    'header-max-length': [0],
+  },
+};
+
